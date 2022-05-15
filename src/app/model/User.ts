36f56018df5ecade05e0ad845de2034c4db51323
KasunDissanayake <kasunprageethdissanayake@gmail.com>
2022-05-15 10:04:56 +0530

    Initial  commit

---
+++ src/app/model/User.ts
@@ -0,0 +1,20 @@
+export class User {
+
+  firstName: string;
+  lastName: string;
+  telephoneNumber: string;
+  nic: string;
+  email: string;
+  userID: string = '';
+  isVerified: boolean;
+  id: string ;
+  userType: string;
+  // profileImage: string;
+
+
+  // getData(): object {
+  //   const result = {};
+  //   Object.keys(this).map(key => result[key] = this[key]);
+  //   return result;
+  // }
+}
